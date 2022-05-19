ebff86717c1f0c2d63c146fc98235ccd394df2a8
Mahima SIngh <mahima.ms0007@gmail.com>
2022-05-19 17:34:58 +0200

    Particle System and sound effects

--- CWC-Prototype3/Assets/Scripts/PlayerController.cs
+++ CWC-Prototype3/Assets/Scripts/PlayerController.cs
@@ -8,7 +8,16 @@ public class PlayerController : MonoBehaviour
 
     private Animator playerJump;
 
+    public ParticleSystem explosion;
+
+    public ParticleSystem dirtSplatter;
+
+    public AudioClip JumpSound;
+
+    public AudioClip Crash;
+
     private Rigidbody PlayerRb;
+    private AudioSource playerAudio;
     public float jump;
     public float gravityModifier;
     private bool isOnGround = false;
@@ -18,6 +27,8 @@ public class PlayerController : MonoBehaviour
         PlayerRb = GetComponent<Rigidbody>();
         playerJump = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +39,9 @@ public class PlayerController : MonoBehaviour
             PlayerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
             isOnGround = false;
             playerJump.SetTrigger("Jump_trig");
+            dirtSplatter.Stop();
+
+            playerAudio.PlayOneShot(JumpSound, 1.0f);
 
         }
     }
@@ -37,13 +51,17 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
             Debug.Log(isOnGround);
+            dirtSplatter.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            Debug.Log("Game Over");
+            //Debug.Log("Game Over");
             gameOver = true;
             playerJump.SetBool("Death_b", true);
             playerJump.SetInteger("DeathType_int", 1);
+            explosion.Play();
+            dirtSplatter.Stop();
+            playerAudio.PlayOneShot(Crash, 1.0f);
         }
     }
 }
